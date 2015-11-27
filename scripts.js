3adf61addf2b575eaeae1a6ecffaedc5ef9ebd0f
Theodore Messinezis <theo@theomessin.com>
2015-11-27 22:48:03 +0200

    Inital add function coding

--- scripts.js
+++ scripts.js
@@ -1,9 +1,16 @@
 window.calculator = window.calculator || {};
 
 window.calculator.global = {
-  init: function(){
-    console.log("Calculator Initialized...");
-  },
+	init: function(){
+		console.log("Calculator Initialized...");
+	},
+	add: function(operands){
+		var sum = 0;
+  		operands.forEach(function(operand) {
+    			sum += operand;
+  		});
+		return sum;
+  	}
 };
 
 $( window ).load(function() {

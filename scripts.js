1d06d50545fed28fb40c752b054e5bedc64bc115
Redoc Tsuj <Redoc.Tsuj@gmail.com>
2015-11-28 19:15:27 +0200

    Added Min feature

--- scripts.js
+++ scripts.js
@@ -1,8 +1,59 @@
+"use strict";
+
+
+function isBlank(o){return isEmpty(o);}
+function notBlank(o){return ! isBlank(o);}
+function notEmpty(o){return ! isEmpty(o);}
+
+function isObj(o){ 
+	var t=typeof(o);
+	var e=true;
+	var v=o;
+	t=t.toLowerCase();
+	if (t=="string") {
+		e=false
+	} else if ((t=="number") || (t=="function")) {
+		e=false; 
+	} else if (t=="object")  {
+		e=true; 
+	} else if ((v===null) || (v===undefined) ){
+		e=false;
+	} else if (! isEmpty(v.length) ){
+		e=true;
+	} else {
+		var c=0;
+		for (var x in v){c+=1;}
+		e=(c==0);
+	}
+	return e;
+}
+
+function isEmpty(o){
+	var t=typeof(o);
+	var e=true;
+	var v=o;
+	t=t.toLowerCase();
+	if (t=="string") v=o.trim();
+	if ((t=="number") || (t=="function")) {
+		e=false; 
+	} else if ((v===null) || (v===undefined) ){
+		e=true;
+	} else if (! isEmpty(v.length) ){
+		e=v.length==0;
+	} else {
+		var c=0;
+		for (var x in v){c+=1;}
+		e=(c==0);
+	}
+	return e;
+}
+
+
 var stats_class=function(array){
 	var intArray = new Array();
 	array.forEach(
 		function(item){ 
-			if (item && item!="" && ! isNaN(item)) { 
+			if (notEmpty(item) && ! isNaN(item)) { 
 				intArray.push(parseInt(item));
 			} else {
 				console.log("[" + item + "] is not a proper number");
@@ -28,6 +79,17 @@ stats_class.prototype.average=function(){
   			return "Can not calc avg";
 }
 
+stats_class.prototype.min=function(){
+		var min ;
+  		this.values.forEach(function(v) {
+			  if (isEmpty(min)) {
+				  min=v
+			  } else {
+				 if (v<min) min=v; 
+			  }
+  		});
+		return min;
+}
 function doAdd(listAsString){
 	var stats=new stats_class( listAsString.split(","));
 	alert(stats.sum());
@@ -36,4 +98,9 @@ function doAdd(listAsString){
 function doAvg(listAsString){
 	var stats=new stats_class( listAsString.split(","));
 	alert(stats.average());
+}
+
+function doMin(listAsString){
+	var stats=new stats_class( listAsString.split(","));
+	alert(stats.min());
 }
